3c0956fd2ae8ae6122b2d3a77946571a20fe3e32
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-18 12:09:31 -0600

    updated the get route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -16,18 +16,23 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     Post.findOne({
-        where: { id: req.params.id },
+        where: {
+            id: req.params.id
+        },
         attributes: ['id', 'post_url', 'title', 'created_at'],
-        include: [{
-            model: User,
-            attributes: ['username']
-        }]
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
         .then(dbPostData => {
             if (!dbPostData) {
                 res.status(404).json({ message: 'No post found with this id' });
                 return;
             }
+            res.json(dbPostData);
         })
         .catch(err => {
             console.log(err);
