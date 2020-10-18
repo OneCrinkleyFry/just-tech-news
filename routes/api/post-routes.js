f382dc35d8e201c510911bb859f7ffa8c9c0aad6
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-18 12:25:01 -0600

    added the put route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -53,6 +53,22 @@ router.post('/', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    Post.update(req.body, { where: { id: req.params.id } })
+        .then(dbPostData => {
+            if (!dbPostData[0]) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 
 
 module.exports = router;
