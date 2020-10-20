2706adef1d6a8290a29d57d30194f67621d0b295
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-20 11:14:35 -0600

    adjusted the user get to include the comments

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -14,22 +14,31 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: {
-            exclude: ['password'],
-            include: [
-                {
-                    model: Post,
-                    attributes: ['id', 'title', 'post_url', 'created_at']
-                },
-                {
-                    model: Post,
-                    attributes: ['title'],
-                    through: Vote,
-                    as: 'voted_posts'
-                }
-            ]
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
         },
-        where: { id: req.params.id }
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
