8eb44afaea2f3bac27143a30c25ebdbc8761d2a5
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-27 10:56:04 -0600

    added the page for a single post, and rendered it from my DB

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+//main page w/posts
 router.get('/', (req, res) => {
-    console.log(req.session);
 
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at',
@@ -33,9 +33,9 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         })
-
 });
 
+//login page
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -45,6 +45,8 @@ router.get('/login', (req, res) => {
     res.render('login')
 });
 
+
+//logout button actions
 router.get('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
@@ -53,6 +55,39 @@ router.get('/logout', (req, res) => {
     } else {
         res.status(404).end();
     }
+});
+
+//page for a single post
+router.get('/post/:id', (req, res) => {
+    const post = Post.findOne({
+        where: { id: req.params.id },
+        attributes: ['id', 'post_url', 'title', 'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const post = dbPostData.get({ plain: true });
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 module.exports = router;
