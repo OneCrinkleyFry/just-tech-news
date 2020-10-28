87d0422015743d617615f212e5c8b460449f69e7
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-28 12:16:41 -0600

    added the helper to properly pluralize points, and comments

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,16 @@
-const { format_date } = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
-const date = new Date('2020-03-20 16:12:03');
+    const date = new Date('2020-03-20 16:12:03');
 
-expect(format_date(date)).toBe('3/20/2020');
+    expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() returns the correct pluralization depending on quantity', () => {
+    let word = "tiger";
+    let quantity = 2;
+    expect(format_plural(word, quantity)).toBe('tigers');
+    word = "lion";
+    quantity = 1;
+    expect(format_plural(word, quantity)).toBe('lion');
 });
