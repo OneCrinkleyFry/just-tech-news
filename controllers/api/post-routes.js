3d3bd75ad101a5c1a2b54d442eb3b773cd3b3aa9
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-28 23:56:58 -0600

    added the routes for the edit post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     console.log('==============================');
